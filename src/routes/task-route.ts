2162779dc6e2de317151b79fc432fe1dba41f00c
Breno Soriani <breno.soriani@gmail.com>
2025-08-01 14:03:12 -0300

    aplicando no controller

--- src/routes/task-route.ts
+++ src/routes/task-route.ts
@@ -1,3 +1,11 @@
 import { Router } from "express";
+import { TaskController } from "../controller/taskController";
 
 export const taskRoute = Router();
+const taskController = new TaskController();
+
+taskRoute.post("/", taskController.create);
+taskRoute.get("/", taskController.getAll);
+taskRoute.get("/:id", taskController.getById);
+taskRoute.put("/:id", taskController.update);
+taskRoute.delete("/:id", taskController.delete);
